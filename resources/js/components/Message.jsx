dfef2e72ae64b576b6e0aed45fc50c00ec1fbe21
hieu313 <hieunm3103@gmail.com>
2024-02-23 16:27:36 +0700

    Done!

--- resources/js/components/Message.jsx
+++ resources/js/components/Message.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 export default function Message(props, user){
-    console.log(props)
     if (props){
         return props.props.map((prop, i)=> {
             // if ('status' in prop) return false
             return (
-                <div className={(prop.user_id == props.user) ? 'direct-chat-msg right' : 'direct-chat-msg'} style={{paddingTop: "10px"}} key={i}>
+                <div className={(prop.user_id === props.user) ? 'direct-chat-msg right' : 'direct-chat-msg'} style={{paddingTop: "10px"}} key={i}>
                     <div className="direct-chat-infos clearfix">
-                        <span className={(prop.user_id == props.user) ? 'direct-chat-name float-right' : 'direct-chat-name float-left'}>{prop.user}</span>
-                        <span className={(prop.user_id == props.user) ? 'direct-chat-timestamp float-left' : 'direct-chat-timestamp float-right'}>{prop.created_at}</span>
+                        <span className={(prop.user_id === props.user) ? 'direct-chat-name float-right' : 'direct-chat-name float-left'}>{prop.user}</span>
+                        <span className={(prop.user_id === props.user) ? 'direct-chat-timestamp float-left' : 'direct-chat-timestamp float-right'}>{prop.created_at}</span>
                     </div>
                     <img className="direct-chat-img"
                          src="https://images.pexels.com/lib/avatars/grey.png?w=130&h=130&fit=crop&dpr=1"
